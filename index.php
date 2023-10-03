d3ffceb3db3926cd8338439d25d4a54482642724
William Davidson <wj.davidson@outlook.com>
2023-10-03 08:01:18 +0200

    mobile first responsive stylign + calendar navigation buttons

--- index.php
+++ index.php
@@ -17,7 +17,17 @@ $calendar = [
         <button type="submit" name="dateType" value="year" class="date-select <?= ($dateType === 'year') ? 'selected-date' : '' ?>">Year</button>
     </form>
     <section>
-        <h1><?= ucwords($calendar['month']); ?><span class="year-title"><?= ucwords($calendar['year']); ?></span></h1>
+        <nav class="date-nav">
+            <form class="date-selection-container" method="post">
+                <button type="submit" name='dateSelection' value="prevDate"><i class="fa-solid fa-chevron-left"></i></button>
+                <button type="submit" name='dateSelection' value="currentDate">Today</button>
+                <button type="submit" name='dateSelection' value="nextDate"><i class="fa-solid fa-chevron-right"></i></button>
+            </form>
+            <h1><?= ucwords($calendar['month']); ?><span class="year-title"><?= ucwords($calendar['year']); ?></span></h1>
+        </nav>
+        <div>
+
+        </div>
     </section>
 </main>
 <?php require_once __DIR__ . '/footer.php'; ?>
