fc5af0174e4351b42e4a66a5468e1596dc7db237
William Davidson <wj.davidson@outlook.com>
2023-10-05 13:58:10 +0200

    Fixed incorect days for prev month in week view

--- calendarWeek.php
+++ calendarWeek.php
@@ -6,27 +6,34 @@ $weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 $currentDayOffset = date('N', mktime(0, 0, 0, $_SESSION['month'] + 1, $_SESSION['day'], $_SESSION['year']));
 
 // Get days before current day of the week.
+$nextMonthCount = 0;
+
 if ($currentDayOffset > 1) {
+    $prevMonthDays = [];
     for ($i = $currentDayOffset - 1; $i >= 1; $i--) {
         $newDay = $_SESSION['day'] - $i;
         if ($newDay >= 1) {
             $week[] = $newDay;
         } else {
-            $newDay = $calendar[$_SESSION['month'] - 1]['monthLength'] - $i;
-            $week[] = $newDay;
+            $prevMonthDays[] = $calendar[$_SESSION['month'] - 1]['monthLength'] - $nextMonthCount;
+            $nextMonthCount++;
         }
     }
-    sort($week);
+    $week = array_merge(array_reverse($prevMonthDays), $week);
 }
 
-echo '<pre/>';
-print_r($_SESSION['day']);
+// The rest of the week.
+$weekCount = count($week);
+$nextMonthCount = 1;
 
-echo '<pre/>';
-print_r($_SESSION['month']);
-
-echo '<pre/>';
-print_r($_SESSION['year']);
+for ($i = 0; $i < 7 - $weekCount; $i++) {
+    if ($_SESSION['day'] + $i > $calendar[$_SESSION['month']]['monthLength']) {
+        $week[] = $nextMonthCount;
+        $nextMonthCount++;
+    } else {
+        $week[] = $_SESSION['day'] + $i;
+    }
+}
 
 echo '<pre/>';
 print_r($week);
